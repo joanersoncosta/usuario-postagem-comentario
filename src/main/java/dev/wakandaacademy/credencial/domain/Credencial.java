9101a1ed4882b4c526065870a219783d9ec4c6a5
Joanerson Costa <joanersonc31@gmail.com>
2023-11-21 07:41:41 -0300

    atualiza dominio

--- src/main/java/dev/wakandaacademy/credencial/domain/Credencial.java
+++ src/main/java/dev/wakandaacademy/credencial/domain/Credencial.java
@@ -1,6 +1,24 @@
 package dev.wakandaacademy.credencial.domain;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.FieldType;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Document(collection = "Credencial")
 public class Credencial {
+	@MongoId(targetType = FieldType.STRING)
+	@Getter
 	private String usuario;
+	
+	@NotNull
+	@Size(min = 6)
+	@Getter(value = AccessLevel.PRIVATE)
 	private String senha;
 }
