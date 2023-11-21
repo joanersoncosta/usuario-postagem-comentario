07a355a458899d236bb693c826dbb822e17fecdb
Joanerson Costa <joanersonc31@gmail.com>
2023-11-21 10:57:29 -0300

    implementa usuario request e id response

--- src/main/java/dev/wakandaacademy/usuario/application/api/PessoaIdResponse.java
+++ src/main/java/dev/wakandaacademy/usuario/application/api/PessoaIdResponse.java
@@ -1,5 +1,13 @@
 package dev.wakandaacademy.usuario.application.api;
 
+import java.util.UUID;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
 public class PessoaIdResponse {
+	private UUID idUsuario;
 
 }
--- src/main/java/dev/wakandaacademy/usuario/application/api/PessoaNovoRequest.java
+++ src/main/java/dev/wakandaacademy/usuario/application/api/PessoaNovoRequest.java
@@ -1,5 +1,22 @@
 package dev.wakandaacademy.usuario.application.api;
 
-public class PessoaNovoRequest {
+import dev.wakandaacademy.usuario.domain.enuns.Sexo;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
 
+@Getter
+public class PessoaNovoRequest {
+	@NotBlank
+	private String nome;
+	@Email
+	@NotNull
+	private String email;
+	@NotBlank
+	private String telefone;
+	@NotBlank
+	private Sexo sexo;
+	@NotNull
+	private String dataNascimento;
 }
