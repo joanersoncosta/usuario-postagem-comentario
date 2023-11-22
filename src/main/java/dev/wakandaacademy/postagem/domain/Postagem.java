fe4424ad8d516cdd72c52acfbd14b6f210a0f8e9
Joanerson Costa <joanersonc31@gmail.com>
2023-11-22 11:05:28 -0300

    implementa agregado de posatgem

---
+++ src/main/java/dev/wakandaacademy/postagem/domain/Postagem.java
@@ -0,0 +1,53 @@
+package dev.wakandaacademy.postagem.domain;
+
+import java.time.Instant;
+import java.util.Date;
+import java.util.UUID;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.FieldType;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import dev.wakandaacademy.usuario.domain.Usuario;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Document(collection = "Postagem")
+public class Postagem {
+	
+	@Id
+	@MongoId(value = FieldType.STRING)
+	private UUID idPostagem;
+	@Indexed
+	private UUID idUsuario;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "GMT")
+	private Date data;
+	@NotBlank
+	@Size(message = "Campo titlo postagem não pode estar vazio!", min = 3, max = 50)
+	private String titlo;
+	@NotBlank
+	@Size(message = "Campo descrição postagem não pode estar vazio!", min = 3, max = 250)
+	private String descricao;
+	private UsuarioPostagem autor;
+
+	public Postagem(String titlo, String descricao, Usuario usuario) {
+		this.idPostagem = UUID.randomUUID();
+		this.data = Date.from(Instant.now());
+		this.titlo = titlo;
+		this.descricao = descricao;
+		this.autor = new UsuarioPostagem(usuario);
+	}
+}
---
+++ src/main/java/dev/wakandaacademy/postagem/domain/enuns/StatusPostagem.java
@@ -0,0 +1,5 @@
+package dev.wakandaacademy.postagem.domain.enuns;
+
+public enum StatusPostagem {
+	INATIVA, ATIVO;
+}
